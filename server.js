8aaed0f729c7006be044c82662374a58b53c5c9b
MichaelWiss <michael.wiss@gmail.com>
2017-06-12 12:36:00 -0500

    middleware

--- server.js
+++ server.js
@@ -2,11 +2,23 @@
 
 const express = require('express');
 const app = express();
+app.set('port', process.env.PORT || 3000);
+
+let helloMiddleware = (req, res, next) => {
+	req.hello = "Hello! It's me! I was wondering....you get the idea!";
+	next();
+}
+
+app.use(helloMiddleware);
 
 app.get('/', (req, res, next) => {
 	res.send('<h1>Hello Express!</h1>');
 });
 
-app.listen(3000, () => {
-	console.log('Chatcat running on port:', 3000);
+app.get('/dashboard', (req, res, next) => {
+	res.send('<h1>This is the dashboard page!  Middleware says: ' +req.hello + '</h1>');
+});
+
+app.listen(app.get('port'), () => {
+	console.log('Chatcat running on port:', app.get('port'));
 });
