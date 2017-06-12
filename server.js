dd6d9798f3c798766dbc9748424e0fa7a0e203a4
MichaelWiss <michael.wiss@gmail.com>
2017-06-12 17:28:06 -0500

    templating

--- server.js
+++ server.js
@@ -3,11 +3,12 @@
 const express = require('express');
 const app = express();
 app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
 
 
 app.get('/', (req, res, next) => {
 	//res.send('<h1>Hello Express!</h1>');
-	res.sendFile(__dirname + '/views/login.htm');
+	res.render('login');
 });
 
 app.listen(app.get('port'), () => {
