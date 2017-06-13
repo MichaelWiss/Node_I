7161dedff59c77b212cc68f982353daf57c5ac29
MichaelWiss <michael.wiss@gmail.com>
2017-06-13 13:44:46 -0500

    router

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,7 @@
 'use strict';
 const router = require('express').Router();
 
-modeule.exports = () => {
+module.exports = () => {
 	let routes = {
 		'get': {
 			'/': (req, res, next) => {
@@ -22,13 +22,19 @@ modeule.exports = () => {
 	//iterate through the routes object and mount the routes
     let registerRoutes = (routes, method) => {
     	for(let key in routes) {
-    		if(typeof routes[key] ==='object' && routes[key] !=== null && !(routes[key] instanceof Array)) {}
+    		if(typeof routes[key] ==='object' && routes[key] !=== null && !(routes[key] instanceof Array)) {
     			registerRoutes(routes[key], key);
     	} else {
     		// register the routes
     		if(method === 'get') {
     			router.get(key, routes[key]);
-    		}
+    		} else if(method === 'post') {
+    			router.post(key, routes[key]);
+    		} 
     	}
     }
+  }
+    registerRoutes(routes);
+
+    return router;
 }
