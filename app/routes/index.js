c3950f1c611015876f46a2706a6b831b8e9b0549
MichaelWiss <michael.wiss@gmail.com>
2017-06-13 13:27:44 -0500

    iterate over routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -20,5 +20,15 @@ modeule.exports = () => {
 	}
 
 	//iterate through the routes object and mount the routes
-
+    let registerRoutes = (routes, method) => {
+    	for(let key in routes) {
+    		if(typeof routes[key] ==='object' && routes[key] !=== null && !(routes[key] instanceof Array)) {}
+    			registerRoutes(routes[key], key);
+    	} else {
+    		// register the routes
+    		if(method === 'get') {
+    			router.get(key, routes[key]);
+    		}
+    	}
+    }
 }
