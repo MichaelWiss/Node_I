bdaf3ff0c02e0fb9a2ba86831718ff7711423286
MichaelWiss <michael.wiss@gmail.com>
2017-06-14 15:09:41 -0500

    initialize session

--- app/config/index.js
+++ app/config/index.js
@@ -11,4 +11,3 @@ if(process.env.NODE_ENV === 'production') {
 		//offer dev stage settings and data
 		module.exports = require('./development.json');
 	}
-}
--- app/session/index.js
+++ app/session/index.js
@@ -1,3 +1,14 @@
 'use strict';
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
+const config = require('../config');
+
+if(process.env.NODE_ENV === 'production') {
+	// initialize session with settings for production
+} else {
+	//initialize session with settings for dev
+	module.exports = session({
+		secret: config.sessionSecret,
+		resave: false
+	})
+}
