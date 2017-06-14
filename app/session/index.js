42f128e1f732e9c19c8c9b8bc5aff29dc4c346bf
MichaelWiss <michael.wiss@gmail.com>
2017-06-14 15:15:17 -0500

    connect to mongoose n session

--- app/session/index.js
+++ app/session/index.js
@@ -2,13 +2,23 @@
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 const config = require('../config');
+const db = require('../db');
 
 if(process.env.NODE_ENV === 'production') {
 	// initialize session with settings for production
+	module.exports = session({
+		secret: config.sessionSecret,
+		resave: false,
+		saveUninitialized: false,
+		store: new MongoStore({
+			mongooseConnection: db.mongoose.connection
+		})
+	})
 } else {
 	//initialize session with settings for dev
 	module.exports = session({
 		secret: config.sessionSecret,
-		resave: false
-	})
+		resave: false,
+		saveUninitialized: true
+	});
 }
