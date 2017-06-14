0c8a01859451677733f7abb0cd794b898824492d
MichaelWiss <michael.wiss@gmail.com>
2017-06-14 14:49:23 -0500

    sessionSecret

--- app/config/index.js
+++ app/config/index.js
@@ -4,7 +4,8 @@ if(process.env.NODE_ENV === 'production') {
 	//offfer production stage environment variables
 	module.exports = {
 		host: process.env.host || "",
-		dbURI: process.env.dbURI
+		dbURI: process.env.dbURI,
+		sessionSecret: process.env.sessionSecret
 	}
 	else {
 		//offer dev stage settings and data
